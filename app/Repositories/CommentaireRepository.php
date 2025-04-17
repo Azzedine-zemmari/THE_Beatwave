e44f4b736cc2c91101b1fe98631718cb08853f87
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-04-17 21:30:53 +0100

    create a commentarie repository and interface with

---
+++ app/Repositories/CommentaireRepository.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\Repositories;
+
+use App\Models\Commentaire;
+use App\Repositories\Contracts\CommentaireInterface;
+
+class CommentaireRepository implements CommentaireInterface{
+    public function create(array $data)
+    {
+        return Commentaire::create([
+            'eventId' => $data['eventId'],
+            'userId' => $data['userId'],
+            'commentaire' => $data['commentaire']
+        ]);
+    }
+}
---
+++ app/Repositories/Contracts/CommentaireInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface CommentaireInterface{
+    public function create(array $data);
+}
