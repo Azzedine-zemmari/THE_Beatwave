9ce65c483e26ecb89d8ce39bee6453e6e44ba15f
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-04-20 12:36:14 +0100

    add the artistService with get method

---
+++ app/Services/ArtistService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\UserRepositoryInterface;
+
+class ArtistService{
+    private $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function get(){
+        return $this->userRepository->findByRole('artist');
+    }
+
+}
